ee50e88bca26d51c0536a6affa12873c34a74441
agata olczak-peuker <pojker@gmail.com>
2024-02-17 23:27:00 +0100

    commit - Multiplication

--- Program.cs
+++ Program.cs
@@ -7,11 +7,19 @@ Console.WriteLine(c);
 
 
  Output = 24
-*/
+
 
 int a = 18;
 int b = 6;
 int c = a - b;
 Console.WriteLine(c);
 
-// Output = 24
+ Output = 24
+*/
+
+int a = 18;
+int b = 6;
+int c = a * b;
+Console.WriteLine(c);
+
+//Output = 108
