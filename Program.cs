6a3f0bb13c81d9cebd1ae9ae268b664fd6729e33
agata olczak-peuker <pojker@gmail.com>
2024-02-18 00:34:20 +0100

    double type

--- Program.cs
+++ Program.cs
@@ -85,7 +85,7 @@ Console.WriteLine($"remainder: {e}");
 
 //quotient: 3
 //remainder: 2
-*/
+
 
 
 
@@ -93,9 +93,51 @@ int max = int.MaxValue;
 int min = int.MinValue;
 Console.WriteLine($"The range of integers is {min} to {max}");
 
-
 int what = max + 3;
 Console.WriteLine($"An example of overflow: {what}");
 
 //Output: The range of integers is -2147483648 to 2147483647
 //An example of overflow: -2147483646
+*/
+
+
+
+//double type
+int a = 5;
+int b = 4;
+int c = 2;
+int d = (a + b) / c;
+Console.WriteLine(d);
+
+//Output = 4 (in reality 4,5)
+
+double a = 5;
+double b = 4;
+double c = 2;
+double d = (a + b) / c;
+Console.WriteLine(d);
+
+//Output = 4,5
+
+
+double a = 19;
+double b = 23;
+double c = 8;
+double d = (a + b) / c;
+Console.WriteLine(d);
+
+//Output = 5,25
+
+
+double max = double.MaxValue;
+double min = double.MinValue;
+Console.WriteLine($"The range of double is {min} to {max}");
+
+//Output: The range of double is -1.79769313486232E+308 to 1.79769313486232E+308
+
+
+double third = 1.0 / 3.0;
+Console.WriteLine(third);
+
+//Output = 0.333333333333333
+
