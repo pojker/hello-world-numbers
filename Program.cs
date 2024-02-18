a2c005a81b14d258d325859ac9eca78c4bb72c05
agata olczak-peuker <pojker@gmail.com>
2024-02-18 01:06:18 +0100

    decimal types

--- Program.cs
+++ Program.cs
@@ -98,7 +98,7 @@ Console.WriteLine($"An example of overflow: {what}");
 
 //Output: The range of integers is -2147483648 to 2147483647
 //An example of overflow: -2147483646
-*/
+
 
 
 
@@ -141,3 +141,49 @@ Console.WriteLine(third);
 
 //Output = 0.333333333333333
 
+
+
+
+
+//Decimal types
+
+decimal min = decimal.MinValue;
+decimal max = decimal.MaxValue;
+Console.WriteLine($"The range of the decimal type is {min} to {max}");
+
+//Output: The range of the decimal type is -79228162514264337593543950335 to 79228162514264337593543950335
+
+
+//greater precision is with the decimal type:
+
+double a = 1.0;
+double b = 3.0;
+Console.WriteLine(a / b);
+
+decimal c = 1.0M;
+decimal d = 3.0M;
+Console.WriteLine(c / d);
+
+//Output:
+ 0.333333333333333
+ 0.3333333333333333333333333333
+
+
+
+
+// CHALLENGE FROM Work with decimal types:
+write code that calculates the area of a circle whose radius is 2.50 centimeters.
+Remember that the area of a circle is the radius squared multiplied by PI.
+One hint: .NET contains a constant for PI, Math.PI that you can use for that value.
+Math.PI, like all constants declared in the System.Math namespace, is a double value.
+For that reason, you should use double instead of decimal values for this challenge.
+
+You should get an answer between 19 and 20.
+
+double radius = 2.50;
+double pi = Math.PI;
+Console.WriteLine(pi * (radius * radius));
+
+//Output: 19.6349540849362
+
+*/
